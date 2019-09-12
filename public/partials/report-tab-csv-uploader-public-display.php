ea51b8f503a64b9b26443cadde39057e1fd0e961
210262 <rutujakadam0210@gmail.com>
2019-09-12 20:29:01 +0530

    Add files via upload

---
+++ public/partials/report-tab-csv-uploader-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       http://
+ * @since      1.0.0
+ *
+ * @package    Report_Tab_Csv_Uploader
+ * @subpackage Report_Tab_Csv_Uploader/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
